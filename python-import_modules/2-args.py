042a85c25745395968ff77d09beb18973aa43f72
InspectriceColumbo <9949@holbertonstudents.com>
2025-03-14 18:42:57 +0100

    args index

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+         print(f"{num_args} arguments.")
+    elif num_args == 1:
+        print(f"{num_args} argument:")
+    else:
+        print(f"{num_args} arguments:")
+
+    for i, arg in enumerate(sys.argv[1:], 1):
+        print(f"{i}: {arg}")
