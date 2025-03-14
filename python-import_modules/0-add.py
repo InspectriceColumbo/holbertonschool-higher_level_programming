5350833b4e98c034cb33291835d26bd889f89669
InspectriceColumbo <9949@holbertonstudents.com>
2025-03-14 17:23:55 +0100

    first import

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0 as oradd
+if __name__ == "__main__":
+    import add_0 as add
 
 a = 1
 b = 2
-result = oradd.add(a, b)
-print("{:d} + {:d} = ".format(a, b), result)
+print("{:d} + {:d} = {}".format(a, b, add(a, b)))
