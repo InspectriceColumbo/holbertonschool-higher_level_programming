670638659a0b4d3f76063274acee5419230f1d4b
InspectriceColumbo <colombe.deveaudfr@gmail.com>
2025-05-12 18:58:50 +0200

    Delete python-data_structures directory

--- python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    newstring = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            newstring += char
-    return newstring
