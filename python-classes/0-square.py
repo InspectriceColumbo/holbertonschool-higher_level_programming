69a94f2b7897abbbda784ab83d553a15b5c068b7
InspectriceColumbo <9949@holbertonstudents.com>
2025-05-23 17:06:16 +0200

    basic square class

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module defines a basic Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a geometric square.
+
+    For the moment it's a placeholder: it doesn't have any properties nor methods.
+    """
+    pass
