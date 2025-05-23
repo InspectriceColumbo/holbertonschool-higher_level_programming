3c9f42b0925651dc4dd5d0c59a068024f1e69bf6
InspectriceColumbo <9949@holbertonstudents.com>
2025-05-23 17:32:44 +0200

    square size and area

---
+++ python-classes/3-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines a basic Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a geometric square with a given size.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes the square with an optional size (0 by default).
+
+        Arguments:
+            size (int): The size of the square's side.
+
+        Errors:
+            TypeError: Raised if size is not an integer.
+
+            ValueError: Raised if size is less than 0.
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """
+        Returns the square's current area.
+
+        Returns:
+            int: The square's area, calculated by
+                    squaring the size (size^2).
+
+        """
+
+        return self.__size ** 2
