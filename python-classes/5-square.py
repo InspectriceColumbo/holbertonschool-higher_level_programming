24f7a89f3c83da7cf3e9c436ecc4678df63bde0a
InspectriceColumbo <9949@holbertonstudents.com>
2025-05-23 18:08:56 +0200

    print a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+This module defines a basic Square class.
+"""
+
+
+class Square:
+    """
+    This class represents a geometric square with a given size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes the square with an optional size.
+
+        Arguments:
+                size (int): The size of the square's side.
+        """
+
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        Retrieves the square's size.
+
+        Returns:
+                int: The side of the square's side.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Initializes the square with an optional size (0 by default).
+
+        Arguments:
+            size (int): The square's new size.
+
+        Errors:
+            TypeError: Raised if size is not an integer.
+
+            ValueError: Raised if size is less than 0.
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates and returns the square's area.
+
+        Returns:
+                int: The square's area (size^2).
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Displays the square using '#' characters.
+
+        If the square's size is 0, an empty line is printed.
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
